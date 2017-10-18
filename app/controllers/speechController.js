0eb36c07b1ab8a6aa79a35163e397ffcd3257123
Bradawk <niepceron.brad@gmail.com>
2017-10-18 15:09:57 +0200

    GPIO lib

--- app/controllers/speechController.js
+++ app/controllers/speechController.js
@@ -1,19 +1,16 @@
-let wpi = require('wiring-pi');
-let configPin = 7;
+let Gpio = require('onoff').Gpio;
+let led = new Gpio(4, 'out');
 
 
-exports.handleSpeechRequest = (req, res) =>{
-    wpi.setup('wpi');
-    wpi.pinMode(configPin, wpi.OUTPUT);
-
-    let isOn    = 0;
+exports.handleSpeechRequest = (req, res) =>{ 
     let action  = req.body.action;
-
+    
     if(action == 'light_on'){
-        isOn = 1;
-        wpi.digitalWrite(configPin, isLedOn);
+        led.writeSync(1);
         res.send()
     }else{
+        led.writeSync(0);
+        led.unexport();
         res.send()
     }
 }
